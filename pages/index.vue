13d9646cffe6a2d30fac8c93d916767fe7fd6447
sanjay <jadems321@gmail.com>
2021-10-08 16:29:27 +0545

    chore: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  data() {
+    return {
+      blog: {
+        title: ""
+      }
+    }
+  },
+  head() {
+    return {
+      title: this.blog.title
+    }
+  }
+})
+</script>
